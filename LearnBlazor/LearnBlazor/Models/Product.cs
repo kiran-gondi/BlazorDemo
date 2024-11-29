d8337303f17e20d82a14433d35951f73e37b1e4d
kiran.gondi <kiran.gondi@planview.com>
2024-11-29 23:16:57 +0530

    Dropdown in Blazor

--- LearnBlazor/LearnBlazor/Models/Product.cs
+++ LearnBlazor/LearnBlazor/Models/Product.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public double Price { get; set; }
         public bool IsActive { get; set; }
+
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
     }
 }
