5a4e16517a2fbc4c21cb39d086f821d8f5ab1b71
kiran.gondi <kiran.gondi@planview.com>
2024-12-30 22:33:21 +0530

    Unit Test Added for the CategoryRepository Methods

--- YumBlazor/YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/YumBlazor/Data/ApplicationDbContext.cs
@@ -3,7 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace YumBlazor.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : IdentityDbContext<ApplicationUser>(options)
     {
+        public virtual DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { Id = 1, Name="Appetizer"},
+                    new Category { Id = 2, Name="Entree"},
+                    new Category { Id = 3, Name="Dessert"}
+                );
+        }
     }
 }
---
+++ YumBlazor/YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<Category> Create(Category category);
+        public Task<Category> Update(Category category);
+        public Task<bool> Delete(int categoryId);
+        public Task<IEnumerable<Category>> GetAll();
+        public Task<Category> Get(int categoryId);
+    }
+}
